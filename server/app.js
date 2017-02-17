107d0056e749e305c8799c1ccbb7f1939143cb3e
Dexter Tong <dyt217@nyu.edu>
2017-02-17 14:55:20 -0500

    Removed unused packages

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
@@ -17,7 +16,6 @@ app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', routes);
